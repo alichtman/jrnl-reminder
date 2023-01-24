ed8b05cc86567b278050a6e718f5ad577b2608f8
Aaron Lichtman <aaronlichtman@gmail.com>
2023-01-24 05:41:18 -0800

    Simplify logic and update deps

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 #!/usr/bin/env node
 
 const execa = require("execa");
-const notifier = require("node-notifier");
-const nc = new notifier.NotificationCenter();
-
+const nodeNotifier = require('node-notifier');
 const jrnl = (...args) => execa("jrnl", args);
 
 (async () => {
-  const desiredFrequency = "30 minutes"
-  const recentEntries = (await jrnl("-from", `${desiredFrequency} ago`, "--export", "json")).stdout;
-  const { entries } = JSON.parse(recentEntries);
+    const desiredFrequency = "1 minute"
+    const recentEntries = (await jrnl("-from", `${desiredFrequency} ago`)).stdout;
+    if (recentEntries != "") {
+        console.log(`jrnl has entries within the last ${desiredFrequency}`);
+        return;
+    }
 
-  if (entries.length > 0) {
-    console.log(`Has entries from last ${desiredFrequency}`);
-    return;
-  }
-
-  nc.notify({
-    title: "Journal reminder",
-    message: "You haven't added a jrnl entry in a while. What are you working on?",
-    closeLabel: "Not now",
-    reply: true,
-    wait: true,
-    timeout: 60
-  });
-
-  nc.on("replied", (obj, options, metadata) => {
-    const message = metadata.activationValue;
-    jrnl(message);
-  });
+    var notifier = new nodeNotifier.Notification();
+    notifier.notify({
+        title: "jrnl reminder",
+        message: "You haven't added a jrnl entry in a while. Write one now?",
+        closeLabel: "Not now",
+        reply: true,
+        wait: true,
+        timeout: 60
+    },
+        function (err, response, metadata) {
+            jrnl(response);
+        }
+    );
 })();
